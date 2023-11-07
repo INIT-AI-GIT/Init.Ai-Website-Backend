151f759909f7e92e554a7e133e7936b093961ba3
Gurjit Singh <gurjit9320@gmail.com>
2023-11-07 22:22:18 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const achievementRouter = require("./routes/Achievement");
 const PORT = process.env.PORT || 5000;
 const DB = process.env.DATABASE 
@@ -9,6 +10,7 @@ const app = express()
 
 
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use(achievementRouter)
 
